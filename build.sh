bdcbd9bee47291623cd30791458ade11bea87d0a
Anton <af001@users.noreply.github.com>
2022-11-24 22:26:53 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+VERSION="v0.1.2"
 
 # Build multi-arch binaries
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o build/linux_amd64/jeb
@@ -10,12 +11,12 @@ GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o build/darwin_arm64/jeb
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o build/windows_amd64/jeb
 
 # Compress
-tar -czvf jeb_linux_amd64.tgz -C build/linux_amd64 .
-tar -czvf jeb_linux_arm64.tgz -C build/linux_arm64 .
-tar -czvf jeb_linux_arm.tgz -C build/linux_arm .
-tar -czvf jeb_darwin_amd64.tgz -C build/darwin_amd64 .
-tar -czvf jeb_darwin_arm64.tgz -C build/darwin_arm64 .
-tar -czvf jeb_windows_amd64.tgz -C build/windows_amd64 .
+tar -czvf jeb-linux-amd64-$VERSION.tgz -C build/linux_amd64 .
+tar -czvf jeb-linux-arm64-$VERSION.tgz -C build/linux_arm64 .
+tar -czvf jeb-linux-arm-$VERSION.tgz -C build/linux_arm .
+tar -czvf jeb-darwin-amd64-$VERSION.tgz -C build/darwin_amd64 .
+tar -czvf jeb-darwin-arm64-$VERSION.tgz -C build/darwin_arm64 .
+tar -czvf jeb-windows-amd64-$VERSION.tgz -C build/windows_amd64 .
 
 # Get sha1sum from binaries
 mv build/*/*.tgz .
